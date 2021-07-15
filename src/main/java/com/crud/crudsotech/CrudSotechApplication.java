2f8fe89ea7a89f74ce7ad2e70f62cee940f3bede
André Rocha <andreroch546@gmail.com>
2021-07-15 16:43:55 -0300

    first commit

---
+++ src/main/java/com/crud/crudsotech/CrudSotechApplication.java
@@ -0,0 +1,13 @@
+package com.crud.crudsotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSotechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudSotechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crud/crudsotech/CrudSotechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.crudsotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSotechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

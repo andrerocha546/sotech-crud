70a4e3afec4a2bc3ce579b94b3271778a1a9f78a
André Rocha <andreroch546@gmail.com>
2021-07-16 14:10:49 -0300

    refact: fix @Document

--- src/main/java/com/crud/crudsotech/entities/Patient.java
+++ src/main/java/com/crud/crudsotech/entities/Patient.java
@@ -6,7 +6,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@Document(collation = "patient")
+@Document("patient")
 public class Patient extends BaseEntity {
 
     @Id

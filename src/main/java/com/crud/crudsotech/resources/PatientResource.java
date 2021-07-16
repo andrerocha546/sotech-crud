2a352f81590ce5d24d167cf1ed46719421b35e92
André Rocha <andreroch546@gmail.com>
2021-07-16 09:52:09 -0300

    feat: find patient by id

--- src/main/java/com/crud/crudsotech/resources/PatientResource.java
+++ src/main/java/com/crud/crudsotech/resources/PatientResource.java
@@ -3,10 +3,9 @@ package com.crud.crudsotech.resources;
 import com.crud.crudsotech.entities.Patient;
 import com.crud.crudsotech.services.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/")
@@ -18,6 +17,11 @@ public class PatientResource {
     @PostMapping
     public Patient insert(@RequestBody Patient patient) {
         return patientService.insert(patient);
+    }
+
+    @GetMapping("{id}")
+    public Optional<Patient> findById(@PathVariable("id") String patientId) {
+        return patientService.findById(patientId);
     }
 
 }
--- src/main/java/com/crud/crudsotech/services/PatientService.java
+++ src/main/java/com/crud/crudsotech/services/PatientService.java
@@ -5,6 +5,8 @@ import com.crud.crudsotech.repositories.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class PatientService {
 
@@ -15,4 +17,7 @@ public class PatientService {
         return repository.save(patient);
     }
 
+    public Optional<Patient> findById(String patientId) {
+        return repository.findById(patientId);
+    }
 }

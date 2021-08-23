6a0dec4afa89d1b19feb0eaca8bd6fbb3ddc4b70
andrerocha546 <andrerocha546@gmail.com>
2021-08-23 14:33:25 -0300

    test(UserService): save a user

---
+++ backend/src/test/java/com/crud/crudsotech/builder/PatientBuilder.java
@@ -0,0 +1,45 @@
+package com.crud.crudsotech.builder;
+
+import com.crud.crudsotech.entities.Patient;
+import com.crud.crudsotech.entities.data.Father;
+import com.crud.crudsotech.entities.data.Mother;
+import com.crud.crudsotech.entities.data.Parents;
+import lombok.Builder;
+
+import java.util.Date;
+
+@Builder
+public class PatientBuilder {
+
+    @Builder.Default
+    private String name = "Patient Test";
+    @Builder.Default
+    private Date birthDate = new Date();
+    @Builder.Default
+    private Parents parents = setParents();
+
+    private static Parents setParents() {
+        Father father = new Father();
+        father.setName("Father Test");
+        father.setBirthDate(new Date());
+
+        Mother mother = new Mother();
+        mother.setName("Mother Test");
+        mother.setBirthDate(new Date());
+
+        Parents parents = new Parents();
+        parents.setMother(mother);
+        parents.setFather(father);
+
+        return parents;
+    }
+
+    public Patient toPatient() {
+        Patient patient = new Patient();
+        patient.setName(this.name);
+        patient.setBirthDate(this.birthDate);
+        patient.setParents(this.parents);
+        return patient;
+    }
+
+}
---
+++ backend/src/test/java/com/crud/crudsotech/services/PatientServiceTest.java
@@ -0,0 +1,57 @@
+package com.crud.crudsotech.services;
+
+import com.crud.crudsotech.builder.PatientBuilder;
+import com.crud.crudsotech.entities.Patient;
+import com.crud.crudsotech.repositories.PatientRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class PatientServiceTest {
+
+    @Mock
+    private PatientRepository repository;
+    @InjectMocks
+    private PatientService service;
+    private Patient patient;
+
+    @BeforeEach
+    void setUp() {
+        this.patient = PatientBuilder.builder().build().toPatient();
+    }
+
+    @Test
+    public void whenAPatientWereSavedItShouldReturnThePatient() {
+        when(repository.save(any())).thenReturn(patient);
+
+        Patient patientCreated = service.insert(patient);
+
+        assertAll(
+                () -> assertEquals(patient.getName(), patientCreated.getName()),
+                () -> assertEquals(patient.getBirthDate(), patientCreated.getBirthDate()),
+                () -> {
+                    assertEquals(patient.getParents().getFather().getName(),
+                            patientCreated.getParents().getFather().getName());
+                    assertEquals(patient.getParents().getFather().getBirthDate(),
+                            patientCreated.getParents().getFather().getBirthDate());
+                },
+                () -> {
+                    assertEquals(patient.getParents().getMother().getName(),
+                            patientCreated.getParents().getMother().getName());
+                    assertEquals(patient.getParents().getMother().getBirthDate(),
+                            patientCreated.getParents().getMother().getBirthDate());
+                }
+        );
+    }
+
+}
